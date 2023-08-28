86aeb128dc58c8b1ba6e14b8207e1c93aca087de
mumbereh <siirasonh@gmail.com>
2023-08-28 16:24:50 +0300

    Add Redux directory

---
+++ src/components/Redux/store.js
@@ -0,0 +1,13 @@
+
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './booksSlice';
+import categoriesReducer from './categoriesSlice';
+
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
+
+export default store;
